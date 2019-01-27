367abb91478056e670c1319db2253521294d45b9
Brady Claypool <poolofclay33@gmail.com>
2019-01-26 20:24:14 -0600

    UI All Updated

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public GameObject _title;
     public GameObject _team;
+
+    public Slider _slider;
 
     public void LoadScene()
     {
@@ -32,5 +35,15 @@ public class Menu : MonoBehaviour
     {
         _title.SetActive(false);
         _team.SetActive(false);
+    }
+
+    public void FullScreen()
+    {
+        Screen.fullScreen = !Screen.fullScreen;
+    }
+
+    public void Update()
+    {
+        AudioListener.volume = _slider.value;
     }
 }
